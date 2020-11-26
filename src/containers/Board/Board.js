af3aae051796a95ebe0c6097ca4410d67ff8e43b
mounvip1525 <mounvip1525@gmail.com>
2020-11-26 10:33:46 +0530

    dnd completed

--- src/containers/Board/Board.js
+++ src/containers/Board/Board.js
@@ -1,25 +1,37 @@
 import React from "react";
 import { useState } from "react";
-import { v4 as uuid } from "uuid";
-import { DragDropContext } from "react-beautiful-dnd";
 import List from "../../components/List/List";
 import classes from "./Board.module.css";
+import { v4 as uuid } from "uuid";
+import { DragDropContext } from "react-beautiful-dnd";
 
 const Board = (props) => {
-	const [inputText, setInputText] = useState("");
+  const [inputText, setInputText] = useState("");
   const [lists, setLists] = useState([
-    {
-      title: "List-one",
-      id: uuid(),
-      cards: [
-        { task: "q", id: uuid() },
-        { task: "w", id: uuid() },
-        { task: "e", id: uuid() },
-        { task: "t", id: uuid() },
-        { task: "y", id: uuid() },
-        { task: "r", id: uuid() },
-      ],
-    },
+    // {
+    //   title: "List-one",
+    //   id: uuid(),
+    //   cards: [
+    //     { task: "q", id: uuid() },
+    //     { task: "w", id: uuid() },
+    //     { task: "e", id: uuid() },
+    //     { task: "t", id: uuid() },
+    //     { task: "y", id: uuid() },
+    //     { task: "r", id: uuid() },
+    //   ],
+    // },
+    // {
+    //   title: "List-two",
+    //   id: uuid(),
+    //   cards: [
+    //     { task: "a", id: uuid() },
+    //     { task: "b", id: uuid() },
+    //     { task: "c", id: uuid() },
+    //     { task: "d", id: uuid() },
+    //     { task: "f", id: uuid() },
+    //     { task: "g", id: uuid() },
+    //   ],
+    // },
   ]);
   function inputValue(event) {
     const newItem = event.target.value;
@@ -83,15 +95,39 @@ const Board = (props) => {
       return;
     }
 
-    let newLists = lists.map((list) =>
-      list.id === source.droppableId
-        ? {
-            ...list,
-            cards: reorderArray(list.cards, source.index, destination.index),
-          }
-        : list
-    );
-    console.log(newLists);
+    const sid = source.droppableId;
+    const did = destination.droppableId;
+    let newLists_beta;
+    let newLists;
+
+    if (sid === did) {
+      newLists = lists.map((list) =>
+        list.id === sid
+          ? {
+              ...list,
+              cards: reorderArray(list.cards, source.index, destination.index),
+            }
+          : list
+      );
+      console.log(newLists);
+    } else {
+      let card;
+      newLists_beta = lists.map((list) => {
+        if (list.id === sid) {
+          let newCards = list.cards;
+          card = newCards.splice(source.index, 1)[0];
+          return { ...list, cards: newCards };
+        } else return list;
+      });
+      newLists = newLists_beta.map((list) => {
+        if (list.id === did) {
+          let newCards = list.cards;
+          newCards.splice(destination.index, 0, card);
+          return { ...list, cards: newCards };
+        } else return list;
+      });
+    }
+
     setLists(newLists);
   }
 
@@ -123,8 +159,6 @@ const Board = (props) => {
             ></List>
           ))}
         </DragDropContext>
-
-
       </div>
     </div>
   );
