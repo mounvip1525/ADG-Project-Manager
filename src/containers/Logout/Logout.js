36fa75da49b1399ac7939ec8537a5b729cdd952a
Aviroop Nandy <60533497+AviroopNandy@users.noreply.github.com>
2020-11-27 21:25:32 +0530

    added Logout.js

---
+++ src/containers/Logout/Logout.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+import LogoutModule from './Logout.module.css';
+
+const Logout = () => {
+    const history = useHistory();
+
+    const handleSubmit = function() {
+        window.localStorage.clear();
+        history.push("/");
+    }
+
+    return(
+        <div className={ LogoutModule.logout }>
+            <button className={ LogoutModule.logoutBtn } onClick={ handleSubmit }>Log-out!</button>
+        </div>
+    )
+}
+
+export default Logout;
