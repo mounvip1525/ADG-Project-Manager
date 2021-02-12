98ea4586292b13939431e412d1fae75347c22d3e
cryptus-neoxys <sharma.dev4242@gmail.com>
2021-02-12 16:12:17 +0530

    dashboard err -WIP

--- src/containers/Dashboard/Dashboard.jsx
+++ src/containers/Dashboard/Dashboard.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import { useState } from "react";
 import { Redirect } from "react-router-dom";
-import { useHistory } from 'react-router-dom';
+// import { useHistory } from "react-router-dom";
 import axios from "axios";
 import Logout from "../Logout/Logout";
 import styles from "./Dashboard.module.css";
@@ -11,65 +11,65 @@ const Dashboard = (props) => {
   // Verify User
 
   // FETCH ALL BOARDS
-  // let [boards, setBoards] = useState([]);
-  // useEffect(() => {
-  //   getAllBoards();
-  // }, []);
+  let [boards, setBoards] = useState([]);
+  useEffect(() => {
+    let config = {
+      method: "GET",
+      url: "https://adg-project-manager.herokuapp.com/api/board/allBoards",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${sessionStorage.getItem("token")}`,
+      },
+    };
+    axios(config)
+      .then((res) => {
+        console.log("data", res.data.allBoards);
+        setBoards(res.data.allBoards);
+        console.log("first", boards);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    console.log("second", boards);
+  }, [boards]);
 
   // uncomment lines 146 if this works out
-  
+
   // function getAllBoards() {
-  //   var config = {
-  //     method: "GET",
-  //     url: "https://adg-project-manager.herokuapp.com/api/board/allBoards",
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //       "Authorization": `Bearer ${sessionStorage.getItem('token')}`
-  //     },
-  //   };
-  //   axios(config)
-  //     .then((response) => {
-  //       console.log("data",response.data.allBoards);
-  //       setBoards(response.data.allBoards);
-  //       console.log("first",boards)
-  //   })
-  //     .catch((error) => {
-  //       console.log(error);
-  //     });
-  //     // console.log("second",boards)
+
   // }
 
-  const[add, show] = useState(false); 
+  const [add, show] = useState(false);
 
-  const[response,setResponse]=useState("");
+  const [response, setResponse] = useState("");
 
   function showAddBoardForm() {
-    show(add => !add);
+    show((add) => !add);
   }
 
   function addBoard(e) {
     e.preventDefault();
-    const name= document.getElementById("name").value;
-    const desc= document.getElementById("desc").value;
+    const name = document.getElementById("name").value;
+    const desc = document.getElementById("desc").value;
 
     const data = JSON.stringify({
       boardName: name,
-      boardDesc: desc
-    })
+      boardDesc: desc,
+    });
 
     var config = {
       method: "post",
       url: "https://adg-project-manager.herokuapp.com/api/board/create",
       headers: {
         "Content-Type": "application/json",
-        "Authorization": `Bearer ${sessionStorage.getItem('token')}`
+        Authorization: `Bearer ${sessionStorage.getItem("token")}`,
       },
       data: data,
     };
 
     axios(config)
-      .then((response) => {
-        console.log("response",response);
+      .then((res) => {
+        console.log("response", res);
         setResponse(response);
       })
       .catch((error) => {
@@ -77,13 +77,10 @@ const Dashboard = (props) => {
       });
   }
 
-
   const isLoggedIn = sessionStorage.getItem("token");
-    if(!isLoggedIn) {
-        return(
-            <Redirect to="/" />
-        )
-    }
+  if (!isLoggedIn) {
+    return <Redirect to="/" />;
+  }
 
   return (
     <div className={styles.container}>
@@ -101,9 +98,25 @@ const Dashboard = (props) => {
             <div className={styles.userRecents}>
               <h4>Recent activities</h4>
               <ul>
-                <li>1. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deserunt qui quidem laboriosam unde totam aspernatur accusamus itaque perferendis vitae, ipsum, sed nostrum mollitia laudantium illum facere, ab quibusdam ad distinctio.</li>
-                <li>2. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad tenetur, ab atque asperiores, excepturi nemo assumenda, inventore commodi soluta facilis repellendus ullam ipsa obcaecati repellat unde ipsam! Praesentium, reprehenderit officiis?</li>
-                <li>3. Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit distinctio eligendi facere laudantium provident temporibus enim ducimus. Ad vel enim eius eum, temporibus at, veniam quibusdam molestiae, non nemo odit.</li>
+                <li>
+                  1. Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                  Deserunt qui quidem laboriosam unde totam aspernatur accusamus
+                  itaque perferendis vitae, ipsum, sed nostrum mollitia
+                  laudantium illum facere, ab quibusdam ad distinctio.
+                </li>
+                <li>
+                  2. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad
+                  tenetur, ab atque asperiores, excepturi nemo assumenda,
+                  inventore commodi soluta facilis repellendus ullam ipsa
+                  obcaecati repellat unde ipsam! Praesentium, reprehenderit
+                  officiis?
+                </li>
+                <li>
+                  3. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Suscipit distinctio eligendi facere laudantium provident
+                  temporibus enim ducimus. Ad vel enim eius eum, temporibus at,
+                  veniam quibusdam molestiae, non nemo odit.
+                </li>
               </ul>
             </div>
           </div>
@@ -116,12 +129,13 @@ const Dashboard = (props) => {
               <h3 className={styles.boardAvatar}>add board</h3>
             </div>
             {add ? (
-                <form>
+              <form>
                 Board Name: <input id="name" name="board" type="text"></input>
-                Board description: <input id="desc" name="desc" type="text"></input>
+                Board description:{" "}
+                <input id="desc" name="desc" type="text"></input>
                 <button onClick={addBoard}>add</button>
               </form>
-               ) : null}
+            ) : null}
             <div className={styles.recentBoards}>
               Recent Boards
               <div className={styles.boardContainer}>
@@ -145,9 +159,11 @@ const Dashboard = (props) => {
                 <div className={styles.boardAvatar}>All 3</div>
                 <div className={styles.boardAvatar}>All 4</div>
                 <div className={styles.boardAvatar}>All 5</div> */}
-                {/* {boards.forEach(board=>{
-                  return <div className={styles.boardAvatar}>{board.boardName}</div>
-                })} */}
+                {boards.forEach((board) => {
+                  return (
+                    <div className={styles.boardAvatar}>{board.boardName}</div>
+                  );
+                })}
               </div>
             </div>
           </div>
